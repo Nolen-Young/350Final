5b7fa70d7659925673e012865d80daa66f8d97b2
nyoung1 <nolen.young@wsu.edu>
2020-05-04 15:17:21 -0700

    Added final touches. working now

--- final.py
+++ final.py
@@ -13,23 +13,25 @@ def main(args):
     # unpack arguments into a tuple
     Q = ((int(args[0]), int(args[1]), int(args[2]), int(args[3])),
          (int(args[4]), int(args[5]), int(args[6]), int(args[7])))
-    print("Equations: {}\n".format(Q))
+    print("Input Equations: {}\n".format(Q))
 
     # find the FA's representing botch equations
     M1 = constructFA(Q[0])
     M2 = constructFA(Q[1])
-    print("M1: {}".format(M1))
-    print("M2: {}\n".format(M2))
+    # print("M1: {}".format(M1))
+    # print("M2: {}\n".format(M2))
 
     # calculate the cartesian product of M1 x M2
     # this will give us our final graph
     M = computeCP(M1, M2)
-    print("M: {}\n".format(M))
+    # print("M: {}\n".format(M))
 
     # Find a walk along M, if a walk exists, return yes, else no
     kc1 = findKC(Q[0][3])
+    print(kc1)
     kc2 = findKC(Q[1][3])
-    test = DFS(M, ((0, 1), (0, 1)), ((0, kc1 + 1), (0, kc2 + 1)))
+    print(kc2)
+    test = DFS(M, ((0, 1), (0, 1)), ((0, kc1+1), (0, kc2+1)))
     print("Path: {}".format(test))
 
     if test:
@@ -40,30 +42,38 @@ def main(args):
     return 1
 
 
+# this function calls dfs helper to so do a dfs on M starting at init
+# and ending at accepting. It will record the path the dfs takes as it goes
 def DFS(M, init, accept):
     visited = set()
     path = ""
+    print(accept)
     path = DFSHelper(visited, M, init, accept, path)
+
+    print(path)
     return path
 
 
+# recursively performs dfs on graph starting at node to accepting
+# records path along the way
 def DFSHelper(visited, graph, node, accept, path):
     if node not in visited:
         visited.add(node)
         for neighbour in graph[node]:
-                newPath = "{}, {}".format(path, neighbour[0])
-                print(newPath)
-                if neighbour[1] == accept:
-                    return newPath
-                else:
-                    DFSHelper(visited, graph, neighbour[1], accept, newPath)
-
+            newPath = "{}, {}".format(path, neighbour[0])
+            print(neighbour[1])
+            if neighbour[1] == accept:
+                return newPath
+            else:
+                return DFSHelper(visited, graph, neighbour[1], accept, newPath)
+    else:
+        return ""
 
 
 # this algorithm computes the cartesian product of two finite
 # automata's, m1 and m2.
 def computeCP(M1, M2):
-    M = {}
+    M = {} # our final solution
 
     # loop through each node in M1, then through edge in that node
     # then find all matching labeled edges in M2, and add that composite
@@ -72,7 +82,7 @@ def computeCP(M1, M2):
         for edge1 in edges1:
             for node2, edges2 in M2.items():
                 for edge2 in edges2:
-                    if (edge1[0] == edge2[0]):
+                    if edge1[0] == edge2[0]:
                         node = (node1, node2)
                         edge = (edge1[0], (edge1[1], edge2[1]))
                         if node in M.keys():
@@ -100,9 +110,9 @@ def constructFA(eq):
                 for a1 in range(2):
                     for a2 in range(2):
                         for a3 in range(2):
-                            bi = b[i - 2] # INDEX MIGHT BE WRONG *************************************************
+                            bi = b[i - 2]  # INDEX MIGHT BE WRONG *************************************************
                             R = (eq[0] * a1) + (eq[1] * a2) + (eq[2] * a3) + bi + carry
-                            if (R % 2 == 0 and carryP == R / 2): # test if it is an edge in the FA
+                            if (R % 2 == 0 and carryP == R / 2):  # test if it is an edge in the FA
                                 iP = i
                                 if (i >= 1 and i <= kc):
                                     iP = iP + 1
